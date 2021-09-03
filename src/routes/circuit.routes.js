c9d2e14d9877ce111e515ec8e37f3e70351cbd45
kyothiago <toberle2@hotmail.com>
2021-09-03 15:42:50 -0300

    atualização de post upload

--- src/routes/circuit.routes.js
+++ src/routes/circuit.routes.js
@@ -1,19 +1,21 @@
 const Router = require("express");
 const cors = require('cors')
 const multer = require('multer')
+const path = require("path");
 
 const circuitRouter = Router();
 
-const circuit = require("../model/Circuit");
+const Circuit = require("../model/Circuit");
 
 circuitRouter.post("/add", (req, res) => {
-  circuit
+  Circuit
     .create({
-      circuitoRef: req.body.referencia,
-      circuitoNome: req.body.nome,
-      circuitoLocalizacao: req.body.localizacao,
-      circuitoPais: req.body.pais,
-      circuitoUrl: req.body.url,
+      circuitoRef: req.body.nameCircuit,
+      circuitoNome: req.body.nameCircuit,
+      circuitoLocalizacao: req.body.local,
+      circuitoPais: req.body.country,
+      circuitoUrl: req.body.urlCircuit,
+      circuitoUrl: req.body.time,
       circuitoFoto: req.body.foto,
     })
     .then((dados) => {
@@ -25,7 +27,7 @@ circuitRouter.post("/add", (req, res) => {
 });
 
 circuitRouter.delete("/delete/:id", (req, res) => {
-  circuit
+  Circuit
     .destroy({
       where: { id: req.params.id },
     })
@@ -38,7 +40,7 @@ circuitRouter.delete("/delete/:id", (req, res) => {
 });
 
 circuitRouter.get("/get", (req, res) => {
-  circuit
+  Circuit
     .findAll()
     .then((dados) => {
       res.json(dados);
@@ -48,25 +50,54 @@ circuitRouter.get("/get", (req, res) => {
     });
 });
 
-/* circuitRouter.put("/upload", upload.single('file'), async (req, res) => {
-  console.log(`Arquivo recebido: ${req.files.length}`);
-  circuit
-    .update(
-      {
-        circuitoFoto: req.body.circuitoFoto,
-      },
-      {
-        where: {
-          circuitoId: req.body.circuitoId,
-        },
-      }
-    )
-    .then(() => {
-      res.status(200).send("Sucesso");
-    })
-    .catch(() => {
-      res.status(400).send("Ocorreu um erro");
+const uuid4 = require("uuid").v4;
+const storage = multer.diskStorage({
+  destination: path.join(__dirname, "public/uploads"),
+  filename: function (req, file, cb) {
+    const fullName =
+      "blog_" + uuid4().replace(/-/g, "") + path.extname(file.originalname);
+    cb(null, fullName);
+  },
+});
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 2000000 }, // file size two milion bytes are allowed
+  fileFilter: function (req, file, cb) {
+    // filter file when it is needed
+    const fileTypes = /png|jpeg|jpg/;
+    const extName = fileTypes.test(path.extname(file.originalname));
+    file.originalname.toLowerCase();
+    const mimeType = fileTypes.test(file.mimetype);
+    if (extName && mimeType) {
+      cb(null, true);
+    } else {
+      cb("Error: only png, jpeg, and jpg are allowed!");
+    }
+  },
+});
+
+circuitRouter.post("/circuits/upload/:id", upload.single("photo"), async (req, res) => {
+  try {
+    const photo = "/uploads/" + req.file.filename;
+    const { id } = req.params;
+    const foto = await Circuit.findOneAndUpdate({ _id: id }, { photo });
+    if (foto == null) {
+      fs.unlink(path.join(__dirname, photo), (err) => {
+        if (err) throw err;
+        else res.status(404).json({ success: false });
+      });
+      return;
+    }
+    res.json({
+      success: true,
+      foto,
     });
-}); */
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      err,
+    });
+  }
+});
 
 module.exports = circuitRouter;
