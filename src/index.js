cb0a4d7290c89ab1fe243c37e1ac349ac02152ee
Thiago Oberle de Almeida <toberle2@hotmail.com>
2021-09-06 21:36:10 -0300

    atualização

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const routes = require("./routes");
 const cors = require("cors");
-
 const app = express();
+const path = require('path')
 
+app.use(express.static(path.join(__dirname, 'uploads')))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(express.static('./public/uploads'))
 app.use(routes);
 
 const connection = require("./database/connection");
